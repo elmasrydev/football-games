1c9eded8a0c7f1c2e1347438320820b256ed07f1
elmasry.dev <elmasry.dev@Gmail.com>
2026-02-09 17:19:18 +0200

    read the videos again

--- resources/views/games/bw/play.blade.php
+++ resources/views/games/bw/play.blade.php
@@ -286,7 +286,7 @@
             // YouTube IFrame API
             if (youtubeId) {
                 const tag = document.createElement('script');
-                tag.src = "https://www.youtube-nocookie.com/iframe_api";
+                tag.src = "https://www.youtube.com/iframe_api";
                 const firstScriptTag = document.getElementsByTagName('script')[0];
                 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
