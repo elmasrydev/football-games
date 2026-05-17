14524119ea23acd419dd2b2e464e91bbe4ed9da6
elmasry.dev <elmasry.dev@Gmail.com>
2026-05-17 11:01:21 +0300

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,16 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3K9YNH5F49"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-3K9YNH5F49');
+    </script>
+
     @stack('styles')
 </head>
 
